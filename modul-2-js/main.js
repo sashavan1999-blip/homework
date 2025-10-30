e5ebd2b8ee8c5758295798ac8113415f8ed2b76c
sashavan1999-blip <sasha.van1999@gmail.com>
2025-10-30 19:45:36 +0300

    Add files via upload

---
+++ modul-2-js/main.js
@@ -0,0 +1,20 @@
+let user = {
+    name: 'Alexander',
+    heil: function(name) {
+        return `Hi ${name}!`;
+    }
+};
+let users = [
+    {name: 'Diana', role: 'user'},
+    {name: 'Ioann', role: 'user'},
+    {name: 'Alexandr', role: 'admin'},
+    {name: 'Senya', role: 'user'}
+];
+let defUsr = 0;
+for (let i = 0; i < users.length; i++) {
+    if (users[i].role === 'user') {
+        defUsr++;
+    }
+}
+console.log(defUsr)
+
