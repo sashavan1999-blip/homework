0a008c77b72776452391a048ad2e212e5e523fb0
sashavan1999-blip <sasha.van1999@gmail.com>
2025-11-13 20:03:18 +0300

    Add files via upload

--- wake_bake/js/main.js
+++ wake_bake/js/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 
     document.addEventListener('click', burgerInit)
     function burgerInit(e) {
@@ -15,5 +15,24 @@
             document.body.classList.remove('body--opened-menu')
         }
     }
+    const btnGift = document.querySelector('.about__img-button')
+    const modal = document.querySelector('.modal')
 
-})()
+    btnGift.addEventListener('click', modalOpen)
+    modal.addEventListener('click', modalClose)
+
+    function modalOpen(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+    function modalClose (e) {
+        e.preventDefault()
+        const target = e.target
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')){
+            document.body.classList.remove('body--opened-modal')
+
+        }
+    }
+
+})
+    () 
