de2e5ea2c82035adc869eb9a0d4d5800710396e9
sashavan1999-blip <sasha.van1999@gmail.com>
2025-11-28 21:33:14 +0300

    Add files via upload

---
+++ wake_bake-4/wake_bake/js/main.js
@@ -0,0 +1,159 @@
+(function () {
+    // БУРГЕР===================
+    document.addEventListener('click', burgerInit)
+    function burgerInit(e) {
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+
+        if (document.documentElement.clientWidth > 900) return
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+    }
+    // МОДАЛКА++++++++++++++++++++++++++++++++++++=
+    const btnGift = document.querySelector('.about__img-button')
+    const modal = document.querySelector('.modal')
+
+    btnGift.addEventListener('click', modalOpen)
+    modal.addEventListener('click', modalClose)
+
+    function modalOpen(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+    function modalClose(e) {
+        e.preventDefault()
+        const target = e.target
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+
+        }
+    }
+    // ТАБКОНТЕНТ========================================
+    const tabControls = document.querySelector('.tab-controls')
+    tabControls.addEventListener('click', toggleTab)
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        activeControl.classList.remove('tab-controls__link--active')
+        tabControl.classList.add('tab-controls__link--active')
+
+        activeContent.classList.remove('tab-content--show')
+        tabContent.classList.add('tab-content--show')
+    }
+    // АККОРДЕОН 
+
+
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el => {
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+
+        });
+
+    });
+    // Сдайдер-галерея
+
+    const swiperGallery = new Swiper('.gallery__slider', {
+        spaceBetween: 15,
+        slidesPerView: 1.5,
+
+        pagination: {
+            el: '.gallery__pagination',
+            type: 'fraction',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.gallery__next',
+            prevEl: '.gallery__prev',
+        },
+        breakpoints: {
+
+            601: {
+                slidesPerView: 3,
+            },
+            801: {
+                spaceBetween: 32,
+            },
+            1101: {
+                slidesPerView: 4,
+                spaceBetween: 32,
+            }
+        }
+    });
+
+    // Сдайдер-отзывы
+    const swiperTestimon = new Swiper('.testimonials__slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
+            }
+
+        }
+    });
+
+        // Маска телефона
+
+        const Telinputs = document.querySelectorAll('input[type="tel"]')
+        const im = new Inputmask('+7 (999) 999-99-99')
+        im.mask(Telinputs)
+
+
+
+
+})() 
